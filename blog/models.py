49916f63279559006ad7955003e5878a8c611495
Javier <jrey1977@yahoo.es>
2023-02-28 23:29:06 +0100

    Primer commit

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Título")
+    content = models.TextField(verbose_name="Contenido")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Creado")
+    modified = models.DateTimeField(auto_now=True, verbose_name="Modificado")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Entrada"
+        verbose_name_plural = "Entradas"
